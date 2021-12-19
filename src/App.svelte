def4d4d372c6def28685613e499b043f41a16808
Dr. Montasir <montasir1400@gmail.com>
2021-12-19 06:46:25 +0300

    Data Table

--- src/App.svelte
+++ src/App.svelte
@@ -3,7 +3,6 @@
 
   import { Line, Bar, Radar } from 'svelte-chartjs';
 
-  // 2D Rotation Matrix
   const ROTATION_MATRIX_2D = mnjs.range(0, 360, 15);
 
   // Color Manipulation
@@ -16,7 +15,7 @@
   const pointRadius = mnjs.rib(4, 11);
 
   // Chart Data
-  const data = {
+  let data = {
     labels: ROTATION_MATRIX_2D,
     datasets: [
       {
@@ -102,6 +101,32 @@
   </div>
 </div>
 
+<br />
+
+<div class="chart">
+  <h3>Data Table</h3>
+  <div class="divTable blueTable" style="text-align: center; margin: 0 auto">
+    <div class="divTableHeading">
+      <div class="divTableRow">
+        <div class="divTableHead">
+          &nbsp;X=[sin<sup>−1</sup>(Y)]°
+        </div>
+        <div class="divTableHead">&nbsp;Y=sin(X°)</div>
+      </div>
+    </div>
+    <div class="divTableBody">
+      {#each ROTATION_MATRIX_2D as X}
+        <div class="divTableRow">
+          <div class="divTableCell">&nbsp;{X}</div>
+          <div class="divTableCell">
+            &nbsp;{mnjs.zeros(mnjs.sin.deg(X), 10)}
+          </div>
+        </div>
+      {/each}
+    </div>
+  </div>
+</div>
+
 <div style="margin: 100px 50px;">
   <h3>Links:</h3>
   <ul>
@@ -165,5 +190,75 @@
     max-width: 800px;
     min-width: 400px;
     margin: 0 auto;
+  }
+
+  /* Data Table */
+  div.blueTable {
+    border: 1px solid #1c6ea4;
+    background-color: #eeeeee;
+    width: 100%;
+    text-align: left;
+    border-collapse: collapse;
+  }
+  .divTable.blueTable .divTableCell,
+  .divTable.blueTable .divTableHead {
+    border: 1px solid #aaaaaa;
+    padding: 3px 2px;
+  }
+  .divTable.blueTable .divTableBody .divTableCell {
+    font-size: 13px;
+  }
+  .divTable.blueTable .divTableRow:nth-child(even) {
+    background: #d0e4f5;
+  }
+  .divTable.blueTable .divTableHeading {
+    background: #1c6ea4;
+    background: -moz-linear-gradient(
+      top,
+      #5592bb 0%,
+      #327cad 66%,
+      #1c6ea4 100%
+    );
+    background: -webkit-linear-gradient(
+      top,
+      #5592bb 0%,
+      #327cad 66%,
+      #1c6ea4 100%
+    );
+    background: linear-gradient(
+      to bottom,
+      #5592bb 0%,
+      #327cad 66%,
+      #1c6ea4 100%
+    );
+    border-bottom: 2px solid #444444;
+  }
+  .divTable.blueTable .divTableHeading .divTableHead {
+    font-size: 15px;
+    font-weight: bold;
+    color: #ffffff;
+    border-left: 2px solid #d0e4f5;
+  }
+  .divTable.blueTable .divTableHeading .divTableHead:first-child {
+    border-left: none;
+  }
+  .divTable {
+    display: table;
+  }
+  .divTableRow {
+    display: table-row;
+  }
+  .divTableHeading {
+    display: table-header-group;
+  }
+  .divTableCell,
+  .divTableHead {
+    display: table-cell;
+  }
+  .divTableHeading {
+    display: table-header-group;
+  }
+  .divTableBody {
+    display: table-row-group;
   }
 </style>
